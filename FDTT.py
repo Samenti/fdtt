398e7dfe457c379a2a9480b0aed63b18342175c4
Raznor <62889081+Raznor@users.noreply.github.com>
2020-06-03 14:04:16 +0200

    Initial commit

---
+++ FDTT.py
@@ -0,0 +1,17 @@
+"""
+Finnish Declension Tables Tester
+--------------------------------
+This is a simple GUI-based python program that helps with
+learning Finnish nominal declension tables.
+"""
+
+# Max line length w/ --- w/o flowing text blocks (PEP 8 style guide): 72 --- 79
+
+import Tkinter as Tk
+
+root = Tk.Tk()
+
+w = Tk.Label(root, text="Hello, world!")
+w.pack()
+
+root.mainloop()
