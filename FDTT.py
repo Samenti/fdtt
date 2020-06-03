8ddacbc9582e517ecf772c411a79a55da36574bd
Raznor <62889081+Raznor@users.noreply.github.com>
2020-06-03 21:24:45 +0200

    created basic layout
    using the grid geometry manager

--- FDTT.py
+++ FDTT.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 """
 Finnish Declension Tables Tester
 --------------------------------
@@ -11,7 +13,40 @@ import Tkinter as Tk
 
 root = Tk.Tk()
 
-w = Tk.Label(root, text="Hello, world!")
-w.pack()
+title_text0 = Tk.Label(root, text="<current word>", font=("Helvetiva", 32))
+title_text0.grid(row=0, column=0)
+title_text1 = Tk.Label(root, text=" – ", font=("Helvetiva", 32))
+title_text1.grid(row=0, column=1)
+title_text2 = Tk.Label(root, text="English translation", font=("Helvetiva", 32))
+title_text2.grid(row=0, column=2)
+
+table_header0 = Tk.Label(root, text="", font=("Helvetiva", 14))
+table_header0.grid(row=1, column=0)
+table_header1 = Tk.Label(root, text="singular", font=("Helvetiva", 14))
+table_header1.grid(row=1, column=1)
+table_header2 = Tk.Label(root, text="plural", font=("Helvetiva", 14))
+table_header2.grid(row=1, column=2)
+
+entry_dic = {}
+labels = [
+"nominative", "genitive", "accusative", "partitive", "illative", "inessive",
+"allative", "adessive", "elative", "ablative", "essive", "abessive",
+"translative", "instructive", "comitative"
+]
+
+row_counter = 2
+for label in labels:
+	entry_dic[label] = (Tk.Label(root), Tk.Entry(root), Tk.Entry(root))
+
+	entry_dic[label][0].config(text=label, font=("Helvetica", 14))
+	entry_dic[label][0].grid(row=row_counter, column=0)
+
+	entry_dic[label][1].config(font=("Helvetica", 14), foreground="darkgray")
+	entry_dic[label][1].grid(row=row_counter, column=1, pady=5)
+
+	entry_dic[label][2].config(font=("Helvetica", 14), foreground="darkgray")
+	entry_dic[label][2].grid(row=row_counter, column=2, pady=5)
+
+	row_counter += 1
 
 root.mainloop()
