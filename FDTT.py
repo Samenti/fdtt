2d31a68757c47c2c27f45199732722b2e88be206
Raznor <62889081+Raznor@users.noreply.github.com>
2020-06-05 00:06:04 +0200

    layout changes, added some test data, initializer function
    layout changes:
    - title is now managed by the packer
    - padding for the title frame is now dynamically calculated
    test data:
    - the data structure should remain in place but I should implement a way to grab new declension data from Wiktionary.org
    initializer function
    - incomplete
    - samples 10 or less words from the declension tables data, sets up answer flags and such

--- FDTT.py
+++ FDTT.py
@@ -10,27 +10,39 @@ learning Finnish nominal declension tables.
 # Max line length w/ --- w/o flowing text blocks (PEP 8 style guide): 72 --- 79
 
 import Tkinter as tk
+import random
+
+declension_tables = [(
+	'äiti', 'äidin', 'äiti', 'äidin', 'äitiä', 'äitiin', 'äidissä',
+	'äidille', 'äidillä', 'äidistä', 'äidiltä', 'äitinä', 'äidittä', 'äidiksi', '', '',
+	'äidit', 'äitien', 'äidit', '', 'äitejä', 'äiteihin', 'äideissä',
+	'äideille', 'äideillä', 'äideistä', 'äideiltä', 'äiteinä', 'äideittä', 'äideiksi', 'äidein', 'äiteineen', 'mother'
+	), ('sokeri', 'sokerin', 'sokeri', 'sokerin', 'sokeria', 'sokeriin', 'sokerissa',
+	'sokerille', 'sokerilla', 'sokerista', 'sokerilta', 'sokerina', 'sokeritta', 'sokeriksi', '', '',
+	'sokerit', ('sokerien', 'sokereiden', 'sokereitten'), 'sokerit', '', ('sokereita', 'sokereja'), 'sokereihin', 'sokereissa',
+	'sokereille', 'sokereilla', 'sokereista', 'sokereilta', 'sokereina', 'sokereitta', 'sokereiksi', 'sokerein', 'sokereineen', 'sugar'
+	), ('suola', 'suolan', 'suola', 'suolan', 'suolaa', 'suolaan', 'suolassa',
+	'suolalle', 'suolalla', 'suolasta', 'suolalta', 'suolana', 'suolatta', 'suolaksi', '', '',
+	'suolat', ('suolojen', 'suolainrare'), 'suolat', '', 'suoloja', 'suoloihin', 'suoloissa',
+	'suoloille', 'suoloilla', 'suoloista', 'suoloilta', 'suoloina', 'suoloitta', 'suoloiksi', 'suoloin', 'suoloineen', 'salt')]
 
 root = tk.Tk()
+title_var = tk.StringVar()
+word_var = tk.StringVar()
 
-word_var=tk.StringVar()
-word_var.set("<current word>")
-english_var=tk.StringVar()
-english_var.set("<English translation>")
+frm_title = tk.Frame(root, bg="steel blue")
+frm_title.pack()
+frm_table = tk.Frame(root, pady=50, bg="light steel blue")
+frm_table.pack()
+lbl_title = tk.Label(frm_title, font=("Helvetica", 32), textvariable=title_var, bg="steel blue")
+lbl_title.pack()
 
-lbl_title_word = tk.Label(root, font=("Helvetiva", 32), textvariable=word_var)
-lbl_title_word.grid(row=0, column=0)
-lbl_title_dash = tk.Label(root, text=" – ", font=("Helvetiva", 32))
-lbl_title_dash.grid(row=0, column=1)
-lbl_title_english = tk.Label(root, font=("Helvetiva", 32), textvariable=english_var)
-lbl_title_english.grid(row=0, column=2)
-
-lbl_table_null = tk.Label(root, text="", font=("Helvetiva", 14))
-lbl_table_null.grid(row=1, column=0)
-lbl_table_sg = tk.Label(root, text="singular", font=("Helvetiva", 14))
-lbl_table_sg.grid(row=1, column=1)
-lbl_table_pl = tk.Label(root, text="plural", font=("Helvetiva", 14))
-lbl_table_pl.grid(row=1, column=2)
+lbl_table_null = tk.Label(frm_table, text="", font=("Helvetica", 14), background="light steel blue")
+lbl_table_null.grid(row=0, column=0)
+lbl_table_sg = tk.Label(frm_table, text="singular", font=("Helvetica", 14), background="light steel blue")
+lbl_table_sg.grid(row=0, column=1)
+lbl_table_pl = tk.Label(frm_table, text="plural", font=("Helvetica", 14), background="light steel blue")
+lbl_table_pl.grid(row=0, column=2)
 
 table_dic = {}
 labels = [
@@ -39,24 +51,26 @@ labels = [
 "translative", "instructive", "comitative"
 ]
 
-row_counter = 2
+row_counter = 1
 for label in labels:
-	table_dic[label] = (tk.Label(root), tk.Entry(root), tk.Entry(root))
+	table_dic[label] = (tk.Label(frm_table), tk.Entry(frm_table), tk.Entry(frm_table))
 
-	table_dic[label][0].config(text=label, font=("Helvetica", 14))
-	table_dic[label][0].grid(row=row_counter, column=0, sticky=tk.W, padx=100)
+	table_dic[label][0].config(text=label, font=("Helvetica", 14), background="light steel blue")
+	table_dic[label][0].grid(row=row_counter, column=0, sticky=tk.W, pady=5, padx=50)
 
 	table_dic[label][1].config(
-		font=("Helvetica", 14), foreground="darkgray",
-		insertbackground="darkgray", insertontime=500, insertofftime=500
+		font=("Helvetica", 14),
+		insertbackground="darkgray", insertontime=500, insertofftime=500,
+		readonlybackground="light steel blue"
 	)
 	table_dic[label][1].grid(row=row_counter, column=1, pady=5)
 
 	table_dic[label][2].config(
-		font=("Helvetica", 14), foreground="darkgray",
-		insertbackground="darkgray", insertontime=500, insertofftime=500
+		font=("Helvetica", 14),
+		insertbackground="darkgray", insertontime=500, insertofftime=500,
+		readonlybackground="light steel blue"
 		)
-	table_dic[label][2].grid(row=row_counter, column=2, pady=5)
+	table_dic[label][2].grid(row=row_counter, column=2, pady=5, padx=50)
 
 	row_counter += 1
 
@@ -68,4 +82,61 @@ table_dic["comitative"][1].insert(0, "–")
 table_dic["instructive"][1].config(state="readonly", justify=tk.CENTER)
 table_dic["comitative"][1].config(state="readonly", justify=tk.CENTER)
 
+def init_game():
+	"""
+	Function to help initialize a game.
+	"""
+	global words, answers, answered, word_var, title_var
+	max_words = len(declension_tables)
+	if max_words < 10:
+		words = random.sample(declension_tables, max_words)
+	else:
+		words = random.sample(declension_tables, 10)
+
+	# print words
+	answers = words.pop()
+	# print answers
+	answered = [False for answer in answers]
+	answered[0] = True
+	word_var.set(answers[0])
+	title_var.set(answers[0] + "   –   " + answers[32])
+	if answers[13] == '':
+		table_dic["instructive"][1].insert(0, "–")
+		table_dic["instructive"][1].config(state="readonly", justify=tk.CENTER)
+		answered[13] = True
+	if answers[14] == '':
+		table_dic["comitative"][1].insert(0, "–")
+		table_dic["comitative"][1].config(state="readonly", justify=tk.CENTER)
+		answered[14] = True
+
+def print_stuff(event):
+	"""Print some info while in root.mainloop()"""
+	print "frm_table.grid_bbox()", frm_table.grid_bbox()
+
+def return_press(event):
+	"""
+	Event handler for an 'Enter key' press.
+	"""
+	pass
+
+def tab_press(event):
+	"""
+	Event handler for a 'tab key' press.
+	"""
+	pass
+
+def resize_title(window_width):
+	"""
+	Function that resizes the title based on width of the title words
+	and the desired width of the window ('window_width').
+	"""
+	label_width = lbl_title.winfo_reqwidth()
+	frm_title.config(padx = (window_width - label_width) / 2) 
+	frm_table.config(padx = (window_width - frm_table.grid_bbox()[2]) / 2)
+
+
+init_game()
+root.bind("<Return>", print_stuff)
+root.wait_visibility()
+resize_title(frm_table.grid_bbox()[2])
 root.mainloop()
